655f2f2c0931c86e29488b6036a92bf2483efcc9
Dave Russell <dave.kerr@gmail.com>
2015-08-24 23:06:46 +0100

    Added subscriptions

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,19 @@
 class SubscribersController < ApplicationController
+
+  def create
+    @subscriber = Subscriber.subscribe(subscriber_params[:email])
+
+    if @subscriber.persisted?
+      redirect_to :back, success: "You've sucessfully signed up to notifications"
+    else
+      redirect_to :back, danger: "Unable to subscribe to notifications"
+    end
+  end
+
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
 end
