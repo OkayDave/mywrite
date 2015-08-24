d618b8bd9b7cde66e2167dfe0cb8498ae1d79f34
Dave Russell <dave.kerr@gmail.com>
2015-08-24 19:56:43 +0100

    added Subscriber.subscribe

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,7 @@
 class Subscriber < ActiveRecord::Base
+
+
+  def self.subscribe(email)
+    return Subscriber.find_or_create_by(email: email)
+  end
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -3,37 +3,42 @@ require 'rails_helper'
 RSpec.describe Subscriber, type: :model do
   describe '.subscribe' do
     context 'new email address' do
-      let(:email) { Faker::Internet.email}
-      let(:subscriber) { Subscriber.subscribe(email) }
+      before do
+        @email = Faker::Internet.email
+        @subscriber = Subscriber.subscribe(@email) 
+      end
 
       it "returns a subscriber object" do
-        expect(subscriber).to be_a(Subscriber)
+        expect(@subscriber).to be_a(Subscriber)
       end
 
       it "stores the email address" do
-        expect(subscriber.email).to eq(email)
+        expect(@subscriber.email).to eq(@email)
       end
 
       it "is the only record for this email" do
-        expect(Subscriber.where(email: email).count).to eq(1)
+        expect(Subscriber.where(email: @email).count).to eq(1)
       end
 
     end
 
     context 'existing email address' do
-      let(:email) { Faker::Internet.email }
-      let(:existing_subscriber) { Subscriber.subscribe(email) }
+      before do
+        @email = Faker::Internet.email
+        @existing_subscriber = Subscriber.subscribe(@email)
+      end
+
 
       it "returns a subscriber object" do
-        expect(Subscriber.subscribe(email)).to be_a(Subscriber)
+        expect(Subscriber.subscribe(@email)).to be_a(Subscriber)
       end
 
       it "returns the existing object" do
-        expect(Subscriber.subscribe(email)).to eq(existing_subscriber)
+        expect(Subscriber.subscribe(@email)).to eq(@existing_subscriber)
       end
 
       it "is the only record for this email" do
-        expect(Subscriber.where(email: email).count).to eq(1)
+        expect(Subscriber.where(email: @email).count).to eq(1)
       end
     end
 
