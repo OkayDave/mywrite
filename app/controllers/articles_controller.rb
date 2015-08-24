423db9c80cd8b0f1b9df0ab0a48d3dba6659f74d
Dave Russell <dave.kerr@gmail.com>
2015-08-24 21:35:17 +0100

    tidy container

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,12 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(params[:article])
+  end
 end
