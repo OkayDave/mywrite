0520505c5cb104161d95290d0632dad2a2cd057d
Dave Russell <dave.kerr@gmail.com>
2015-08-25 00:57:23 +0100

    Added comment deletion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-
+//= require darkly/loader
+//= require darkly/bootswatch
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+  def article
+    @article ||= Article.find(params[:article_id])
+  end
 
   def create
-    @article = Article.find(params[:article_id])
     @comment = Comment.new(comment_params)
-    @comment.article = @article
+    @comment.article = article
 
     if @comment.save 
       redirect_to :back, success: "Comment successfully posted"
@@ -11,6 +13,18 @@ class CommentsController < ApplicationController
       render template: "articles/show", danger: "Unable to post comment"
     end
 
+  end
+
+
+  def destroy
+    @comment = article.comments.find(params[:id]) 
+    @comment.destroy!
+
+    if @comment.destroyed?
+      redirect_to :back, success: "Comment was successfully deleted"
+    else
+      redirect_to :back, danger: "Unabled to delete comment"
+    end
   end
 
   private
